7fae6987608dc98a2dc6a44e79cb5fa46dee348b
sali <sali.taymor@gmail.com>
2022-06-21 23:01:49 +0200

    https//all gebaut

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,36 @@
 import "./App.css";
-import {useEffect, useState} from "react"
-import axios from "axios"
-const url = 'http://localhost:3000/nouns'
+import { useEffect, useState } from "react";
+import axios from "axios";
+const url = "http://localhost:3000/all";
 function App() {
-  const [nouns, setNouns]=useState([]); 
-  useEffect(()=>{
-(async () => {
-  setNouns((await axios.get(url)).data);
-})();
-  },[])
-    
-  //wir sollen in backend erlauben dass unsere front End darauf kommen darf before wir uberhaubt anything displaying
-  //cors origin problem
+    const [siteData, setSiteData] = useState({});
+
+    useEffect(() => {
+        (async () => {
+            const _siteData = (await axios.get(url)).data;
+            setSiteData(_siteData);
+            console.log(_siteData)
+        })();
+    }, []);
+
+    //wir sollen in backend erlauben dass unsere front End darauf kommen darf before wir uberhaubt anything displaying
+    //cors origin problem
 
     return (
         <div className="App">
             <div>Testing</div>
-            <div>There are {nouns.length} nouns</div>
-            <ul>{nouns.map((noun, i) => {
-              return <li key={i}>{noun.singular}</li>;
-            })}</ul>
+            {Object.keys(siteData).length === 0 ? (
+                <div>Loading...</div>
+            ) : (
+                <>
+                    <div>There are {siteData.nouns.length} nouns</div>
+                    <ul>
+                        {siteData.nouns.map((noun, i) => {
+                            return <li key={i}>{noun.singular}</li>;
+                        })}
+                    </ul>
+                </>
+            )}
         </div>
     );
 }
