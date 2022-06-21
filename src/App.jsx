b06decb2ba7629e5fad3486a0ed35d98f804a5e5
sali <sali.taymor@gmail.com>
2022-06-21 21:58:58 +0200

    backend-frontend verbunden

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,27 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-
+import "./App.css";
+import {useEffect, useState} from "react"
+import axios from "axios"
+const url = 'http://localhost:3000/nouns'
 function App() {
-  const [count, setCount] = useState(0)
+  const [nouns, setNouns]=useState([]); 
+  useEffect(()=>{
+(async () => {
+  setNouns((await axios.get(url)).data);
+})();
+  },[])
+    
+  //wir sollen in backend erlauben dass unsere front End darauf kommen darf before wir uberhaubt anything displaying
+  //cors origin problem
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    return (
+        <div className="App">
+            <div>Testing</div>
+            <div>There are {nouns.length} nouns</div>
+            <ul>{nouns.map((noun, i) => {
+              return <li key={i}>{noun.singular}</li>;
+            })}</ul>
+        </div>
+    );
 }
 
-export default App
+export default App;
